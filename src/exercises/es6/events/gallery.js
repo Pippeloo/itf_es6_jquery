4ed5fac30eb7d5cab0a2325bc98411601e7796cd
Pippeloo <68497705+Pippeloo@users.noreply.github.com>
2022-03-30 11:37:05 +0200

    Saving exercises

--- src/exercises/es6/events/gallery.js
+++ src/exercises/es6/events/gallery.js
@@ -1,1 +1,13 @@
 // Image gallery
+const thumbnails = document.querySelectorAll('#thumbnails img');
+const mainImage = document.getElementById('fullImage');
+
+thumbnails.forEach(thumbnail => {
+  thumbnail.addEventListener('click', event => {
+      thumbnails.forEach(thumbnail => {
+        thumbnail.classList.remove('border');
+      });
+    thumbnail.classList.add('border');
+    mainImage.src = 'https://picsum.photos/id/' + thumbnail.dataset.id + '/600/400';
+  });
+});
--- src/exercises/es6/events/style_paragraphs.js
+++ src/exercises/es6/events/style_paragraphs.js
@@ -1,1 +1,60 @@
 // Style the paragraphs
+const paragraphs = document.getElementById("paragraphs");
+
+// ===== Font Family =====
+const selector = document.getElementById("font");
+
+selector.addEventListener("change", function(event) {
+  paragraphs.style.fontFamily = event.target.value + "," + "sans-serif";
+});
+
+// ===== Font Size =====
+const sizeSelector = document.getElementsByName("fontsize");
+
+sizeSelector.forEach(function(size) {
+  size.addEventListener('change', function(event) {
+    paragraphs.style.fontSize = event.target.value;
+  });
+});
+
+// ===== Font Style =====
+const bold = document.getElementById("bold");
+const italic = document.getElementById("italic");
+
+bold.addEventListener("click", function() {
+    // check if checkbox is checked
+    if (bold.checked) {
+      paragraphs.style.fontWeight = "bold";
+    } else {
+      paragraphs.style.fontWeight = "normal";
+    }
+});
+
+italic.addEventListener("click", function() {
+   // check if checkbox is checked
+   if (italic.checked) {
+     paragraphs.style.fontStyle = "italic";
+   } else {
+     paragraphs.style.fontStyle = "normal";
+   }
+});
+
+// ===== darkTheme =====
+const darkTheme = document.querySelector('button');
+
+darkTheme.addEventListener('click', function() {
+  if (darkTheme.innerHTML === 'Light theme') {
+    paragraphs.style.backgroundColor = '#f8f8f8';
+    paragraphs.style.color = '#111';
+    darkTheme.innerHTML = 'Dark theme';
+  } else {
+    paragraphs.style.backgroundColor = '#111';
+    paragraphs.style.color = '#f8f8f8';
+    darkTheme.innerHTML = 'Light theme';
+  }
+});
+
+
+
+
+
