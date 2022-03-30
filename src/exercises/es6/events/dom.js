a786d6d64f8aa05528f87e04804a97812f65724e
Pippeloo <68497705+Pippeloo@users.noreply.github.com>
2022-03-30 10:05:28 +0200

    Events dom done

--- src/exercises/es6/events/dom.js
+++ src/exercises/es6/events/dom.js
@@ -1,1 +1,30 @@
 // Basic DOM manipulation
+const lists = document.querySelectorAll('li');
+const header = document.querySelector('h1');
+const button = document.querySelector('button');
+
+lists.forEach(function (element, index) {
+    if (index === 0) {
+        element.addEventListener('click', function () {
+            header.innerHTML = element.innerHTML;
+        });
+    } else if (index === 1) {
+        element.addEventListener('mouseenter', function () {
+            header.classList.add('orange');
+        });
+        element.addEventListener('mouseleave', function () {
+            header.classList.remove('orange');
+        });
+    } else if (index === 2) {
+        element.addEventListener('dblclick', function () {
+            header.classList.toggle('green');
+        });
+    }
+});
+
+button.addEventListener('click', function () {
+    header.style.transform = 'rotate(180deg)';
+});
+button.addEventListener('dblclick', function () {
+    header.style.transform = 'rotate(0deg)';
+});
